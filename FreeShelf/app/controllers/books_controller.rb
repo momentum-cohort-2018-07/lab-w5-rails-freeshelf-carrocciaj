2506981e26038113020b5f74cfe710a30bb9d551
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-27 15:05:41 -0400

    form built everything shows, bug where data doesn't save

--- FreeShelf/app/controllers/books_controller.rb
+++ FreeShelf/app/controllers/books_controller.rb
@@ -1,21 +1,47 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @book = Book.all
   end
 
   def new
-    @books = Book.new
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
+    @book = Book.new(book_params)
+
+    @book.save
+    redirect_to @book
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to @book
   end
+
+  private 
+  def book_params
+    params.permit(:title, :author, :description, :url)
+  end
+
 end
+
