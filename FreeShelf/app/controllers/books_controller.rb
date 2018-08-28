b0eda06db03ac64e421785b1ebbb6d87d05b702f
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-27 22:39:48 -0400

    working crud app

--- FreeShelf/app/controllers/books_controller.rb
+++ FreeShelf/app/controllers/books_controller.rb
@@ -1,47 +1,52 @@
 class BooksController < ApplicationController
-  def index
-    @book = Book.all
-  end
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+def index
+  @book = Book.all
+end
 
-  def new
-    @book = Book.new
-  end
+def show
+  
+end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+def new
+  @book = Book.new
+end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
+def edit
+  
+end
 
-  def create
-    @book = Book.new(book_params)
+def create
+  @book = Book.new(book_params)
 
-    @book.save
+  @book.save
+  redirect_to @book
+end
+
+def update
+  
+
+  if @book.update(book_params)
     redirect_to @book
+  else
+    render 'edit'
   end
+end
 
-  def update
-    @book = Book.find(params[:id])
-
-    if @book.update(book_params)
-      redirect_to @book
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to @book
-  end
+def destroy
+  
+  @book.destroy
+ 
+  redirect_to @book
+end
 
   private 
   def book_params
-    params.permit(:title, :author, :description, :url)
+    params.require(:book).permit(:title, :author, :description, :url)
   end
 
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
 
